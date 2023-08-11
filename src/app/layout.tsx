f432d94889ac92f52ca66516356752bb46961fcb
Medhashis_Maiti <medhashis000@gmail.com>
2023-08-11 23:47:08 +0530

    added buttons and applied the toggle mode for the dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { ThemeProvider } from '@/components/navbar/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className}`}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        
+        {children}
+      </ThemeProvider>
+      </body>
     </html>
   )
 }
