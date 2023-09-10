d58d96c40b81ddeb87a8b0f3d5dd37feaab333a1
Medhashis_Maiti <medhashis000@gmail.com>
2023-09-10 11:56:11 +0530

    settings fixed

--- src/components/resumeUrl/resurl.tsx
+++ src/components/resumeUrl/resurl.tsx
@@ -4,7 +4,7 @@ import { Button } from "../ui/button";
 
 const Resurl = () => {
      // onclick function 
-  const resumeURL = 'https://drive.google.com/file/d/1-FVvIpocdurdhmyOG6Z7JaUZ3NqPVabr/view?usp=drive_link'; // Replace with your actual resume URL
+  const resumeURL = 'https://drive.google.com/file/d/1-FVvIpocdurdhmyOG6Z7JaUZ3NqPVabr/view?usp=sharing'; // Replace with your actual resume URL
 
   const handleClick = () => {
     // Trigger the link when the button is clicked
